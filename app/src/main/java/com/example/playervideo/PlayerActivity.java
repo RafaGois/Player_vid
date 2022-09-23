e4d4b02c5c13734e1645f5e54c5da9beb6fc01ee
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2022-09-23 14:00:34 -0300

    deixa full screen

--- app/src/main/java/com/example/playervideo/PlayerActivity.java
+++ app/src/main/java/com/example/playervideo/PlayerActivity.java
@@ -3,6 +3,7 @@ package com.example.playervideo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -14,6 +15,15 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        //esconde statusbar e barra de navegacao
+        View decorview = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorview.setSystemUiVisibility(uiOptions);
+
+        getSupportActionBar().hide();
 
         videoView = findViewById(R.id.videoView);
         videoView.setMediaController(new MediaController(this));
