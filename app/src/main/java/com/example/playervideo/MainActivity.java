fd38c220a555dd2570e9235af3db9dfe609c6012
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2022-09-23 11:13:22 -0300

    update

--- app/src/main/java/com/example/playervideo/MainActivity.java
+++ app/src/main/java/com/example/playervideo/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.playervideo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void abrirVideo (View view) {
+        Intent intent = new Intent(this,PlayerActivity.class);
+        startActivity(intent);
+    }
 
 }
---
+++ app/src/main/java/com/example/playervideo/PlayerActivity.java
@@ -0,0 +1,24 @@
+package com.example.playervideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    private VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+
+        videoView = findViewById(R.id.videoView);
+        videoView.setMediaController(new MediaController(this));
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.video);
+        videoView.start();
+
+    }
+}
